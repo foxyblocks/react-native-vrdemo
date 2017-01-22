47e28f2c577958d889b085c85b3b34f938948558
Christian Schlensker <christian@cswebartisan.com>
2017-01-22 11:19:30 -0800

    Change organization name

--- ios/VRDemo/AppDelegate.swift
+++ ios/VRDemo/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  VRDemo
 //
 //  Created by Christian Schlensker on 1/21/17.
-//  Copyright © 2017 Facebook. All rights reserved.
+//  Copyright © 2017 Christian Schlensker. All rights reserved.
 //
 
 import UIKit
